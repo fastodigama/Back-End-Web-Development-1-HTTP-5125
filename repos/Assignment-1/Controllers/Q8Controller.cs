662b49c152e4ba46ef5be00190ca587c253b63cb
Fadel Matar <fadel.matar@gmail.com>
2025-01-31 05:39:07 -0500

    modified q8 output format

--- repos/Assignment-1/Controllers/Q8Controller.cs
+++ repos/Assignment-1/Controllers/Q8Controller.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 
@@ -9,18 +10,29 @@ namespace Assignment_1.Controllers
     public class Q8Controller : ControllerBase
     {
         /// <summary>
-        /// Receives an HTTP POST request with a form-encoded body containing two parameters: small and large.
+        /// Handles HTTP POST requests with a form-encoded body containing two parameters: small and large.
         /// </summary>
         /// <param name="small">The quantity of small items.</param>
         /// <param name="large">The quantity of large items.</param>
-        /// <returns>An HTTP response with a body summarizing the order details including subtotal, tax, and total amount.</returns>
+        /// <returns>An HTTP response with a body summarizing the order details including subtotal, tax, and total amount.
+        /// The monetary values are formatted as currency using the current culture.</returns>
         /// <example>
-        /// POST api/Q8/squashfellows
+        /// POST api/QX/squashfellows
         /// HEADERS: Content-Type: application/x-www-form-urlencoded
         /// FORM DATA: "small=2&large=3"
-        /// -> A post method with a form-encoded request body. 
+        /// -> A post method with a form-encoded request body.
         /// Body Content: small: 2, large: 3
+        /// Response Body: "2 Small @ $25.50 = $51.00; 3 Large @ $40.50 = $121.50; Subtotal = $172.50; Tax = $22.43 HST; Total = $194.93"
         /// </example>
+        /// <remarks>
+        /// References:
+        /// <list type="bullet">
+        /// <item>
+        /// <description><a href="https://learn.microsoft.com/en-us/dotnet/standard/base-types/standard-numeric-format-strings#CFormatString">Standard Numeric Format Strings</a></description>
+        /// </item>
+        /// </list>
+        /// </remarks>
+
 
         [HttpPost(template: "squashfellows")]
         [Consumes("application/x-www-form-urlencoded")]
@@ -37,7 +49,9 @@ namespace Assignment_1.Controllers
             double tax = subtotal * HST;
             double totalWithTax = subtotal + tax;
 
-            return $"{small} Small @ $25.50 = ${smallResault}; {large} Large @ $40.50 = ${largeResault}; Subtotal = ${subtotal}; Tax = ${tax} HST; Total = ${totalWithTax}";
+            //reference:https://learn.microsoft.com/en-us/dotnet/standard/base-types/standard-numeric-format-strings#CFormatString
+
+            return $"{small} Small @ $25.50 = {smallResault.ToString("C2",CultureInfo.CurrentCulture)}; {large} Large @ $40.50 = {largeResault.ToString("C2", CultureInfo.CurrentCulture)}; Subtotal = {subtotal.ToString("C2", CultureInfo.CurrentCulture)}; Tax = {tax.ToString("C2", CultureInfo.CurrentCulture)} HST; Total = {totalWithTax.ToString("C2", CultureInfo.CurrentCulture)}";
         }
 
 
--- repos/Assignment-1/obj/Debug/net8.0/Assignment-1.AssemblyInfo.cs
+++ repos/Assignment-1/obj/Debug/net8.0/Assignment-1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb99f9858567c63af218e22cdcc400f9a3b5bd16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a53163f12e2b2e109bd7f557358e9d7085b73611")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
