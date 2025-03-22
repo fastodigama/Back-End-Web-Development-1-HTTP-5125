84203be342a62c1fb180fd78c212ce2481d226cc
Fadel Matar <fadel.matar@gmail.com>
2025-03-22 03:46:14 -0400

    modified some comments

--- repos/Cumulative1/obj/Debug/net8.0/Cumulative1.AssemblyInfo.cs
+++ repos/Cumulative1/obj/Debug/net8.0/Cumulative1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cumulative1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d1355c004ffe3400ea200cceb721d60f93656f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a78860fb1fc9bad483eddf42af6638fc35468a53")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cumulative1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cumulative1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
